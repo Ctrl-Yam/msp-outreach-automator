4eef8924a197e55fe4f612981fc97569f108a7db
Yamkelo Vilakazi <yamkelovilakazi@gmail.com>
2026-08-28 11:23:49 +0200

    Refactor email sending functionality and changed to SSL

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import os
 import smtplib
 from dotenv import load_dotenv
+from email.message import EmailMessage
 
 load_dotenv()
 
-emailAddress = os.environ.get('GMAIL_USER')
-emailPass = os.environ.get('GMAIL_PASSWORD')
+emailAddress = os.environ.get("GMAIL_USER")
+emailPass = os.environ.get("GMAIL_PASSWORD")
+to_emailAddress = os.environ.get("DESTINATION_EMAIL")
 
+msg = EmailMessage()
+msg["Subject"] = "Lets Gooo!"
+msg["From"] = emailAddress
+msg["To"] = to_emailAddress
+msg.set_content("We just won the speedrun")
 
-with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-     smtp.ehlo()
-     smtp.starttls()
-     smtp.ehlo()
-
+with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
      smtp.login(emailAddress, emailPass)
 
-     subject = "FUUUCK!"
-     body = "How about we go for a long walk?"
-
-     msg = f"Subject: {subject} \n\n{body}"
-
-     smtp.sendmail(emailAddress, "destination@gmail.com", msg)
+     smtp.send_message(msg)
