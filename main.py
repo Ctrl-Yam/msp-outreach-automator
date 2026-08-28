a78ae34f9116a37488a5dbeb9d1b7c8b45109810
Yamkelo Vilakazi <yamkelovilakazi@gmail.com>
2026-08-28 16:31:10 +0200

    Update email sending functionality and add resume attachment

--- main.py
+++ main.py
@@ -9,26 +9,39 @@ load_dotenv()
 emailAddress = os.environ.get("GMAIL_USER")
 emailPass = os.environ.get("GMAIL_PASSWORD")
 
-file_path = "C:/Users/Yamkelo/Desktop/msp-outreach-automator/test.csv"
+file_path = "targets.csv"
 
 try:
     with open(file_path, "r") as file:
         content = csv.reader(file)
-        row = list(content)
-        recipient_name = (row[1][3])
-        recipient_email = (row[1][1])
-
-        msg = EmailMessage()
-        msg["Subject"] = "Lets Gooo!"
-        msg["From"] = emailAddress
-        msg["To"] = recipient_email
-        msg.set_content(f"Hi there {recipient_name}")
+        rows = list(content)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
-     
-        smtp.login(emailAddress, emailPass)
+         smtp.login(emailAddress, emailPass)
 
-        smtp.send_message(msg)
+         for row in rows[1:]:
+            recipient_name = row[3]
+            recipient_email = row[1]
+            print(f"emails was sent to {recipient_name}")
+
+            with open("resume.pdf", "rb") as f:
+                 resume_data = f.read()
+                 resume_name = f.name
+
+            msg = EmailMessage()
+            msg["Subject"] = "Lets Gooo!"
+            msg["From"] = emailAddress
+            msg["To"] = recipient_email
+            msg.set_content(f"Hi there {recipient_name}")
+
+            msg.add_attachment(
+                resume_data, 
+                maintype="application", 
+                subtype="pdf", 
+                filename=resume_name
+                )
+
+            smtp.send_message(msg)
 
 except FileNotFoundError:
     print("The file was not found!")
